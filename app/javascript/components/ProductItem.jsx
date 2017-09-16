cdfb9351051be6ee70750666f3922946888e2350
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-16 05:00:01 +0000

    add item component

--- app/javascript/components/ProductItem.jsx
+++ app/javascript/components/ProductItem.jsx
@@ -4,14 +4,35 @@ class ProductItem extends Component {
   
   render(){
       return(
-           
-           <div> 
-                
-               
-
-                
-
-            </div>
+                    <section>
+                         <li className="product-item">
+                            <a className="upvote-button" href="#" >
+                                <span>
+                                    <i className="fa fa-sort-asc"></i>
+                                </span>
+                                <br/>
+                                {this.props.upvote}
+                            
+                            </a>
+                            <img className="product-item-media" src={this.props.media} />
+                            <section className="product-item-info">
+                                <a>
+                                    <h2>{this.props.name}</h2>
+                                </a>
+                                <p>{this.props.description}</p>
+                                <a href="#">
+                                    <img className="small-avatar" src={this.props.makerAvatar}/>
+                                </a>
+                            </section>
+                            <a className="product-item-link" href={this.props.link}></a>
+                            <span>
+                                <i className="fa fa-external-link"></i>
+                            </span>
+                        </li>
+                        
+                        
+                    </section>
+                        
              
           );
       
--- app/javascript/components/ProductList.jsx
+++ app/javascript/components/ProductList.jsx
@@ -6,62 +6,42 @@ class ProductList extends Component {
   render(){
       return(
            
-           <section>
+
             <ul className="product-list">
-                        <li className="product-item">
-                            <a className="upvote-button" href="#" >
-                                <span>
-                                    <i className="fa fa-sort-asc"></i>
-                                </span>
-                                <br/>
-                                {this.props.productList[0].upvote}
-                            
-                            </a>
-                            <img className="product-item-media" src={this.props.productList[0].media} />
-                            <section className="product-item-info">
-                                <a>
-                                    <h2>{this.props.productList[0].name}</h2>
-                                </a>
-                                <p></p>
-                                <a href="#">
-                                    <img className="small-avatar" src={this.props.productList[0].maker.avatar}/>
-                                </a>
-                            </section>
-                            <a className="product-item-link" href={this.props.productList.link}></a>
-                            <span>
-                                <i className="fa fa-external-link"></i>
-                            </span>
-                        </li>
-                    </ul>
-                     <ul className="product-list">
-                        <li className="product-item">
-                            <a className="upvote-button" href="#" >
-                                <span>
-                                    <i className="fa fa-sort-asc"></i>
-                                </span>
-                                <br/>
-                                {this.props.productList[1].upvote}
-                            
-                            </a>
-                            <img className="product-item-media" src={this.props.productList[1].media} />
-                            <section className="product-item-info">
-                                <a>
-                                    <h2>{this.props.productList[1].name}</h2>
-                                </a>
-                                <p></p>
-                                <a href="#">
-                                    <img className="small-avatar" src={this.props.productList[1].maker.avatar}/>
-                                </a>
-                            </section>
-                            <a className="product-item-link" href={this.props.productList.link}></a>
-                            <span>
-                                <i className="fa fa-external-link"></i>
-                            </span>
-                        </li>
-                    </ul>
+                <ProductItem
+                    name = {this.props.productList[0].name}
+                    media = {this.props.productList[0].media}
+                    upvote = {this.props.productList[0].upvote}
+                    makerAvatar = {this.props.productList[0].maker.avatar}
+                    link = {this.props.productList[0].link}
+                    description = {this.props.productList[0].description}
+
+                />    
+                
+                 <ProductItem
+                    name = {this.props.productList[1].name}
+                    media = {this.props.productList[1].media}
+                    upvote = {this.props.productList[1].upvote}
+                    makerAvatar = {this.props.productList[1].maker.avatar}
+                    link = {this.props.productList[1].link}
+                    description = {this.props.productList[1].description}
+
+                />
+            
+            
+                {this.props.productList.map((item, id) => {
+                    return <ProductItem key={id} {...item} />
+                    
+                    
+                })}
+            </ul>
+             
+                 
+                       
            
            
-           </section>
+           
+     
           );
       
   }
