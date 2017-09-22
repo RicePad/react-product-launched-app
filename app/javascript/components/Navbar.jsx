f98fb65f2713d80295a4946138c78904c20dea34
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-22 18:09:40 +0000

    add PostPopup component

--- app/javascript/components/Navbar.jsx
+++ app/javascript/components/Navbar.jsx
@@ -6,59 +6,59 @@ import LoginPopup from './LoginPopup';
 class Navbar extends Component {
     constructor(props){
         super(props);
-        
+
         this.state = {
-          displayPopup: false  
-            
+          displayPopup: false
+
         };
-        
-        
+
+
     }
-    
-    
+
+
     showPopup = () => {
         return this.setState({displayPopup: true});
-        
+
     }
-    
+
     hidePopup = () =>{
         return this.setState({displayPopup: false});
     }
-    
+
     renderProductSearch(){
         return (
             <section className="left-side">
                 <input className="product-search" placeholder="SEARCH" />
             </section>
-            
+
             );
-        
+
     }
-   
+
     renderLogo(){
         return (
             <a  onClick={this.showPopup} ><img src="/images/favicon.ico"/></a>
             );
-        
+
     }
-    
-    
+
+
     renderUser(){
         return (
             <section className="right-side">
                 <a  onClick={this.showPopup} className="login-btn">LOGIN</a>
             </section>
             );
-        
+
     }
-    
-    
-    
-    
+
+
+
+
     render(){
         return(
            <section>
-                
+
             <section className="navbar">
                 {this.renderProductSearch()}
                 {this.renderLogo()}
@@ -66,11 +66,11 @@ class Navbar extends Component {
             </section>
                  <LoginPopup status={this.state.displayPopup} hidePopup={this.hidePopup} />
 
-          </section>    
+          </section>
             );
-        
+
     }
-    
+
 }
 
 export default Navbar;
---
+++ app/javascript/components/PostPopup.jsx
@@ -0,0 +1,39 @@
+import React, { Component }from 'react'
+
+class PostPopup extends Component {
+    render(){
+        return(
+            <Popup {...this.props} style="post-up">
+                <header>Post a new product</header>
+                <section>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>Name</td>
+                                <td><inpout placeholder="Enter product's name" /></td>
+                            </tr>
+                            <tr>
+                                <td>Description</td>
+                                <td><inpout placeholder="Enter product's description" /></td>
+                            </tr>
+                             <tr>
+                                <td>Link</td>
+                                <td><inpout placeholder="www...." /></td>
+                            </tr>
+                            <tr>
+                                <td>Media</td>
+                                <td><inpout placeholder="paste a direct link to an image" /></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+            </Popup>
+            
+            );
+        
+    }
+    
+}
+
+
+export default PostPopup;
