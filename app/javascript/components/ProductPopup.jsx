64a21cd92869b3b73d2f6c5a1a3a6637e686a882
RiskyClick <jonn.thinkrtc@gmail.com>
2017-09-23 03:41:21 +0000

    add style to productpopup component

--- app/javascript/components/ProductPopup.jsx
+++ app/javascript/components/ProductPopup.jsx
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import Popup from './Popup';
 
 class ProductPopup extends Component {
+    constructor() {
+    super();
+    this.state = {
+      product: {
+        id: 2,
+        name: 'ProductLaunched',
+        link: 'https://code4startup.com',
+        media: '/images/code4startup.jpeg',
+        upvote: 278,
+        description: 'Code for starups',
+        maker: {
+          name: 'leo',
+          avatar: '/images/myself.jpg'
+        }
+      }
+    };
+  }
+  
+    renderUpvoteButton() {
+        return (
+          <a className="upvote-button" href="#">
+            <span>
+              <i className="fa fa-sort-asc"></i>
+            </span>
+            {this.state.product.upvote}
+          </a>
+        );
+  }
+
+  renderHeader() {
+    return (
+      <header style={{backgroundImage: 'url(' + this.state.product.media + ')'}}>
+        <section className="header-shadow">
+          <h1>{this.state.product.name}</h1>
+          <p>{this.state.product.description}</p>
+          <section>
+            {this.renderUpvoteButton()}
+            <a className="getit-btn" href={this.state.product.link} target="_blank">GET IT</a>
+          </section>
+        </section>
+      </header>
+    );
+  }
+
+
     
     render(){
         return(
             <Popup {...this.props} style="product-popup">
-                <h2>Product Info</h2>
+                {this.renderHeader()}
             </Popup>
             );
         
